205fe2a0dfe27dc15955017ab85008b49cca49af
dooli1971039 <dooli9819@gmail.com>
2022-07-14 22:47:52 +0900

    feature: make small buton

---
+++ src/components/SmallButton.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {ColorCode} from "../utils/palette";
+
+const StyledButton = styled.button`
+    font-family: "Poppins";
+    font-size: 2.3vmin;
+    // font-weight: bold;
+
+    border: none;
+    border-radius: 15px;
+
+    background: ${ColorCode.BLUE};
+    color: ${ColorCode.WHITE};
+    opacity: 80%;
+    width: 8vw;
+    height: 4vh;
+
+    cursor: pointer;
+    transition: all 0.9s, color 0.3s;
+
+    &:hover {
+        background: ${ColorCode.WHITE};
+        color: ${ColorCode.BLUE};
+        opacity: 100%;
+    }
+`;
+
+const SmallButton = ({text, onClick}) => {
+    return <StyledButton onClick={onClick}>{text}</StyledButton>;
+};
+
+export default SmallButton;
