36cc26fa602af79511a3a52e91dafa3789a7f3a7
dooli1971039 <dooli9819@gmail.com>
2022-07-15 03:46:15 +0900

    feature: Update result page api (axios)

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,4 +1,4 @@
-import {useSearchParams} from "react-router-dom";
+import {useSearchParams, useLocation} from "react-router-dom";
 import CommonButton from "../components/CommonButton";
 import MyHeader from "../components/MyHeader";
 import styled from "styled-components";
@@ -29,39 +29,42 @@ const ResultContainer = styled.div`
 `;
 
 const Result = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const feature_id = searchParams.get("id");
-    const feature_emotion = searchParams.get("emotion");
-
+    // const [searchParams, setSearchParams] = useSearchParams();
+    // const feature_id = searchParams.get("id");
+    // const feature_emotion = searchParams.get("emotion");
+    ////////////
+    const imgImg = useLocation();
     const [imgAddress, setImgAddress] = useState({
-        before: "",
-        after: "",
+        before: imgImg.state.before,
+        after: imgImg.state.after,
     });
 
-    const getData = () => {
-        axios
-            .get(`http://127.0.0.1:8000/cari/result?id=${feature_id}&emotion=${feature_emotion}`)
-            .then((response) => {
-                setImgAddress({
-                    before: response.data[0].before_img,
-                    after: response.data[0].after_img,
-                });
-            })
-            .catch((error) => {
-                // console.log(error);
-                // alert("사진을 가져오는데 실패했습니다.");
-                axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
-                    const before = `${res.data[0].url}`;
-                    const after = `${res.data[1].url}`;
-                    setImgAddress({before, after});
-                });
-            });
-    };
+    // const [imgAddress, setImgAddress] = useState({
+    //     before: "",
+    //     after: "",
+    // });
+    ///////////////
+    // const getData = () => {
+    //     axios
+    //         .get(`http://127.0.0.1:8000/cari/result?id=${feature_id}&emotion=${feature_emotion}`)
+    //         .then((response) => {
+    //             console.log(response);
+    //             // setImgAddress({
+    //             //     before: response.data.before_img,
+    //             //     after: response.data.after_img,
+    //             // });
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //             alert("사진을 가져오는데 실패했습니다.");
+    //             });
+    //         });
+    // };
 
-    useEffect(() => {
-        getData();
-    }, []);
-
+    // useEffect(() => {
+    //     getData();
+    // }, []);
+    ///////////
     return (
         <Container>
             <MyHeader />
