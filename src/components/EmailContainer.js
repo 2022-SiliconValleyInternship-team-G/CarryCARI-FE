308b4a6075360fb82a5a5d637a1a4f3ac24e4931
dooli1971039 <dooli9819@gmail.com>
2022-07-21 04:16:19 +0900

    refactor: Isolate email components

---
+++ src/components/EmailContainer.js
@@ -0,0 +1,127 @@
+import {useNavigate} from "react-router-dom";
+import CommonButton from "../components/CommonButton";
+import styled from "styled-components";
+import SmallButton from "../components/SmallButton";
+import React, {useState, useRef} from "react";
+import axios from "axios";
+
+const EmailContainer1 = styled.div`
+    @media all and (max-width: 600px) and (orientation: portrait) {
+        display: grid;
+        grid-template-rows: 1fr 2fr;
+        place-items: start center;
+    }
+`;
+const EmailSubContainer1 = styled.div`
+    @media all and (max-width: 600px) and (orientation: portrait) {
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        place-items: start center;
+    }
+`;
+const EmailContainer2 = styled.div`
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+    place-items: center center;
+`;
+const Title = styled.div`
+    font-size: 4vmin;
+    font-weight: bold;
+
+    @media all and (max-width: 600px) and (orientation: portrait) {
+        font-size: 7vmin;
+        text-align: center;
+        margin-bottom: 20px;
+    }
+`;
+const InputLine = styled.input`
+    font-family: "Roboto";
+    border-radius: 5px;
+    border: none;
+
+    font-size: 2.5vmin;
+    width: 40vw;
+    margin-right: 10px;
+
+    @media all and (max-width: 600px) and (orientation: portrait) {
+        width: 90vw;
+        font-size: 4vmin;
+        margin: 10px;
+    }
+`;
+
+const EmailContainer = (userFeature) => {
+    const navigate = useNavigate();
+
+    const emailPart = useRef();
+    const [emailInput, setEmailInput] = useState("");
+    const [emailSubmit, setEmailSubmit] = useState(false);
+
+    const submitEmail = () => {
+        if (emailInput.indexOf("@") === -1) {
+            alert("please check your email");
+            emailPart.current.focus();
+        } else {
+            axios
+                .post("http://127.0.0.1:8000/cari/email", {
+                    user_id: userFeature.id,
+                    user_email: emailInput,
+                })
+                .then((response) => {
+                    console.log(response);
+                    if (response.status === 200) {
+                        setEmailSubmit(true);
+                        setEmailInput("");
+                    } else {
+                        alert("Fail to submit Email");
+                        setEmailInput("");
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    alert("Fail to submit Email");
+                    setEmailInput("");
+                });
+        }
+    };
+
+    const handleEmail = (e) => {
+        setEmailInput(e.target.value);
+    };
+
+    return (
+        <>
+            {emailSubmit ? (
+                <EmailContainer2>
+                    <Title>Your email has been submitted successfully.</Title>
+                    <CommonButton
+                        text={"go to the main"}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    />
+                </EmailContainer2>
+            ) : (
+                <EmailContainer1>
+                    <Title>
+                        If you enter your Email,
+                        <br />
+                        we will send the result later
+                    </Title>
+                    <EmailSubContainer1>
+                        <InputLine
+                            placeholder="Enter your Email"
+                            onChange={handleEmail}
+                            type="email"
+                            value={emailInput}
+                            ref={emailPart}
+                        />
+                        <SmallButton text="submit" onClick={submitEmail} />
+                    </EmailSubContainer1>
+                </EmailContainer1>
+            )}
+        </>
+    );
+};
+
+export default React.memo(EmailContainer);
--- src/pages/Waiting.js
+++ src/pages/Waiting.js
@@ -1,11 +1,11 @@
-import {useNavigate, useLocation, useSearchParams} from "react-router-dom";
-import CommonButton from "../components/CommonButton";
+import {useNavigate, useLocation} from "react-router-dom";
 import MyHeader from "../components/MyHeader";
 import styled from "styled-components";
 import Game from "react-chrome-dino";
 import SmallButton from "../components/SmallButton";
-import {useEffect, useState, useRef} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import EmailContainer from "../components/EmailContainer";
 
 const Container = styled.div`
     height: 95vh;
@@ -31,27 +31,14 @@ const GameHeader = styled.div`
     justify-content: space-between;
     align-items: end;
 `;
-const EmailContainer1 = styled.div`
-    @media all and (max-width: 600px) and (orientation: portrait) {
-        display: grid;
-        grid-template-rows: 1fr 2fr;
-        place-items: start center;
-    }
+
+const SubTitle = styled.div`
+    font-size: 2.7vmin;
+    font-weight: bold;
+    text-align: center;
+    margin: 40px;
 `;
 
-const EmailSubContainer1 = styled.div`
-    @media all and (max-width: 600px) and (orientation: portrait) {
-        display: grid;
-        grid-template-rows: 1fr 1fr;
-        place-items: start center;
-    }
-`;
-
-const EmailContainer2 = styled.div`
-    display: grid;
-    grid-template-rows: 1fr 1fr 1fr;
-    place-items: center center;
-`;
 const Title = styled.div`
     font-size: 4vmin;
     font-weight: bold;
@@ -63,30 +50,11 @@ const Title = styled.div`
     }
 `;
 
-const InputLine = styled.input`
-    font-family: "Roboto";
-    border-radius: 5px;
-    border: none;
-
-    font-size: 2.5vmin;
-    width: 40vw;
-    margin-right: 10px;
-
-    @media all and (max-width: 600px) and (orientation: portrait) {
-        width: 90vw;
-        font-size: 4vmin;
-        margin: 10px;
-    }
-`;
-
 const Waiting = () => {
     const navigate = useNavigate();
 
     const userInfo = useLocation();
-    const [userFeature, setUserFeature] = useState({
-        id: userInfo.state.id,
-        emotion: userInfo.state.emotion,
-    });
+    const userFeature = {id: userInfo.state.id, emotion: userInfo.state.emotion};
 
     const [canMove, setCanMove] = useState(false);
     const [imgAddress, setImgAddress] = useState({
@@ -94,48 +62,9 @@ const Waiting = () => {
         after: "",
     });
 
-    const emailPart = useRef();
-    const [emailInput, setEmailInput] = useState("");
-    const [emailSubmit, setEmailSubmit] = useState(false);
-
-    const submitEmail = () => {
-        {
-            if (emailInput.indexOf("@") === -1) {
-                alert("please check your email");
-                emailPart.current.focus();
-            } else {
-                axios
-                    .post("http://127.0.0.1:8000/cari/email", {
-                        user_id: userFeature.id,
-                        user_email: emailInput,
-                    })
-                    .then((response) => {
-                        console.log(response);
-                        console.log(response.status);
-                        if (response.status === 200) {
-                            setEmailSubmit(true);
-                            setEmailInput("");
-                        } else {
-                            alert("Fail to submit Email");
-                            setEmailInput("");
-                        }
-                    })
-                    .catch((error) => {
-                        console.log(error);
-                        alert("Fail to submit Email");
-                        setEmailInput("");
-                    });
-            }
-        }
-    };
-
     const goToResult = () => {
         console.log(imgAddress);
         navigate("/result", {state: imgAddress});
-    };
-
-    const handleEmail = (e) => {
-        setEmailInput(e.target.value);
     };
 
     const getData = () => {
@@ -153,7 +82,7 @@ const Waiting = () => {
             })
             .catch((error) => {
                 console.log(error);
-                //alert("사진을 가져오는데 실패했습니다.");
+                alert("사진을 가져오는데 실패했습니다.");
             });
     };
 
@@ -174,36 +103,9 @@ const Waiting = () => {
                     {canMove ? <SmallButton text="result>>" onClick={goToResult} /> : null}
                 </GameHeader>
                 <Game />
+                <SubTitle>Press the space bar to play the game</SubTitle>
             </GameContainer>
-            {emailSubmit ? (
-                <EmailContainer2>
-                    <Title>Your email has been submitted successfully.</Title>
-                    <CommonButton
-                        text={"go to the main"}
-                        onClick={() => {
-                            navigate("/");
-                        }}
-                    />
-                </EmailContainer2>
-            ) : (
-                <EmailContainer1>
-                    <Title>
-                        If you enter your Email,
-                        <br />
-                        we will send the result later
-                    </Title>
-                    <EmailSubContainer1>
-                        <InputLine
-                            placeholder="Enter your Email"
-                            onChange={handleEmail}
-                            type="email"
-                            value={emailInput}
-                            ref={emailPart}
-                        />
-                        <SmallButton text="submit" onClick={submitEmail} />
-                    </EmailSubContainer1>
-                </EmailContainer1>
-            )}
+            <EmailContainer userFeature={userFeature} />
         </Container>
     );
 };
