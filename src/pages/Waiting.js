dad5985ce13e662fe7b2a8397817f3bae9aa8d90
dooli1971039 <dooli9819@gmail.com>
2022-07-24 04:12:34 +0900

    feature: Check if results are available or not

--- src/pages/Waiting.js
+++ src/pages/Waiting.js
@@ -3,7 +3,7 @@ import MyHeader from "../components/MyHeader";
 import styled from "styled-components";
 import Game from "react-chrome-dino";
 import SmallButton from "../components/SmallButton";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 import EmailContainer from "../components/EmailContainer";
 
@@ -67,28 +67,51 @@ const Waiting = () => {
         navigate("/result", {state: imgAddress});
     };
 
-    const getData = () => {
-        //따로 주기적으로 체크한 뒤에 그 다음에 넘어가기.
-        axios
-            .get(`http://127.0.0.1:8000/cari/result?id=2&emotion=0`)
-            //.get(`http://127.0.0.1:8000/cari/result?id=${userFeature.id}&emotion=${userFeature.emotion}`)
-            .then((response) => {
-                console.log(response);
-                setImgAddress({
-                    before: `http://127.0.0.1:8000${response.data.before_img}`,
-                    after: `http://127.0.0.1:8000${response.data.after_img}`,
+    useInterval(
+        () => {
+            axios
+                //.get(`http://127.0.0.1:8000/cari/result?id=2&emotion=0`)
+                .get(`http://127.0.0.1:8000/cari/result?id=${userFeature.id}&emotion=${userFeature.emotion}`)
+                .then((response) => {
+                    console.log(response);
+                    if (response.data.after_img !== "yet") {
+                        //여기는 종료 조건 (api 수정후 고치기)
+                        setImgAddress({
+                            before: `http://127.0.0.1:8000${response.data.before_img}`,
+                            after: `http://127.0.0.1:8000${response.data.after_img}`,
+                        });
+                        setCanMove(true);
+                    } else {
+                        //여기는 디버깅용
+                    }
+                })
+                .catch((error) => {
+                    console.log(error);
+                    alert("사진을 가져오는데 실패했습니다.");
                 });
-                setCanMove(true);
-            })
-            .catch((error) => {
-                console.log(error);
-                alert("사진을 가져오는데 실패했습니다.");
-            });
-    };
+        },
+        imgAddress.after ? null : 5000
+    );
 
-    useEffect(() => {
-        getData();
-    }, []);
+    function useInterval(callback, delay) {
+        const savedCallback = useRef();
+
+        // Remember the latest function.
+        useEffect(() => {
+            savedCallback.current = callback;
+        }, [callback]);
+
+        // Set up the interval.
+        useEffect(() => {
+            function tick() {
+                savedCallback.current();
+            }
+            if (delay !== null) {
+                let id = setInterval(tick, delay);
+                return () => clearInterval(id);
+            }
+        }, [delay]);
+    }
 
     return (
         <Container>
