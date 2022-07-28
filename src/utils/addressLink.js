e14a35590f5d9ca00ba0c9b6830fbfc5df3819ed
dooli1971039 <dooli9819@gmail.com>
2022-07-28 20:38:11 +0900

    build: Change URL

--- src/utils/addressLink.js
+++ src/utils/addressLink.js
@@ -1,3 +1,3 @@
-//const AddressLink = "http://147.47.125.118:4343";
-const AddressLink = "http://127.0.0.1:8000";
+const AddressLink = "http://147.47.125.118:6006";
+//const AddressLink = "http://127.0.0.1:8000";
 export {AddressLink};
